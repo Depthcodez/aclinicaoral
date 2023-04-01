80372e8acfa44a77ec6fa1d5558cb5dfff89631e
Robert Duran <robertwillian689@gmail.com>
2023-03-31 22:20:47 -0300

    avaliacoes no carrousel index banner e demais ajustes

--- src/resources/js/apiGoogle.js
+++ src/resources/js/apiGoogle.js
@@ -1,25 +1,49 @@
-// ;<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"></script> link para incluir no body
+const placeId = 'ChIJfZbyEzhXzpQRlhDURW4spRU'
+const fields = [
+  'name',
+  'rating',
+  'formatted_phone_number',
+  'geometry',
+  'reviews'
+]
 
-var place_id = 'PLACE_ID'
-var api_key = 'API_KEY'
-
-var request = {
-  placeId: place_id,
-  fields: ['name', 'rating', 'reviews']
-}
-
-var service = new google.maps.places.PlacesService(
+const service = new google.maps.places.PlacesService(
   document.createElement('div')
 )
 
-service.getDetails(request, function (place, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    var good_reviews = place.reviews.filter(function (review) {
-      return review.rating > 3
-    })
+service.getDetails({ placeId, fields }, (place, status) => {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    const reviews = place.reviews
 
-    good_reviews.forEach(function (review) {
-      console.log(review.text)
+    const reviewsList = $('#reviews-carousel')
+
+    if (Array.isArray(reviews)) {
+      reviews
+        .sort((a, b) => b.time - a.time)
+        .forEach(review => {
+          if (review.rating >= 3) {
+            const li = $('<li></li>')
+            li.html(
+              `<div><p><strong>${review.author_name}</strong></p><p>${review.text}</p><p class="rating">${review.rating}/5</p></div>`
+            )
+            reviewsList.append(li)
+          }
+        })
+    } else {
+      const li = $('<li></li>')
+      li.text('Nenhuma avaliação encontrada.')
+      reviewsList.append(li)
+    }
+
+    reviewsList.slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
     })
+  } else {
+    console.error('Erro ao obter informações do local')
   }
 })
