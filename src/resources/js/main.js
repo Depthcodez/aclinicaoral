34b9cf0c6d9c141a5a41956c3086b8a98721d0a0
Robert Duran <robertwillian689@gmail.com>
2023-04-13 14:20:47 -0300

    inclusão de todo html do menu mobile

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -5,3 +5,44 @@ dropdownToggles.forEach(toggle => {
     toggle.parentNode.classList.toggle('active')
   })
 })
+
+// Lógica para menu-mobile
+const tratamentosMobile = document.querySelector('.has-submenu-mobile')
+
+tratamentosMobile.addEventListener('click', function () {
+  this.classList.toggle('active')
+})
+
+const esteticaMobile = document.querySelector('.has-submenu--estetica-mobile')
+
+esteticaMobile.addEventListener('click', function (e) {
+  e.stopPropagation()
+  this.classList.toggle('active')
+})
+
+const cirurgiaMobile = document.querySelector('.has-submenu--cirurgia-mobile')
+
+cirurgiaMobile.addEventListener('click', function (e) {
+  e.stopPropagation()
+  this.classList.toggle('active')
+})
+
+// abrir menu-mobile
+function openNav() {
+  document.getElementById('mySidepanel').style.width = '250px'
+}
+
+function closeNav() {
+  document.getElementById('mySidepanel').style.width = '0'
+}
+
+// logica drop-down-menu-mobile
+const dropdowns = document.querySelectorAll('select')
+dropdowns.forEach(dropdown => {
+  dropdown.addEventListener('change', event => {
+    const selectedOption = event.target.options[event.target.selectedIndex]
+    if (selectedOption.getAttribute('data-href')) {
+      window.location.href = selectedOption.getAttribute('data-href')
+    }
+  })
+})
